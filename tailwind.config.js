4321f792e9cdb1099548a70c5372167501314d5f
Rodrigo <rodrigoaguilar887@gmail.com>
2020-06-17 16:39:45 -0700

    Add development environment config file, and change baseurl to root. Stop including packs.html, and instead load assets directly on head.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+      },
+    },
+  },
+
   plugins: [
     require('@tailwindcss/ui'),
   ]
