a0680dffef53c9dac740770efb4baf82e38952a6
Rodrigo <rodrigoaguilar887@gmail.com>
2020-06-22 15:59:28 -0700

    Add contact page, footer and site-wide navbar.

--- assets/scripts.js
+++ assets/scripts.js
@@ -96,7 +96,8 @@
 var map = {
 	"./button_loader_controller.js": "./_src/controllers/button_loader_controller.js",
 	"./dropdown_controller.js": "./_src/controllers/dropdown_controller.js",
-	"./form_controller.js": "./_src/controllers/form_controller.js"
+	"./form_controller.js": "./_src/controllers/form_controller.js",
+	"./toggle_controller.js": "./_src/controllers/toggle_controller.js"
 };
 
 
@@ -427,8 +428,8 @@ function (_Controller) {
           var alert_template = __webpack_require__(/*! ../handlebars/success_alert.handlebars */ "./_src/handlebars/success_alert.handlebars");
 
           var context = {
-            heading: "Your message was sent successfully",
-            text: "Thanks for sending me a message. I'll get back to you soon."
+            heading: "Your message was successfully sent",
+            text: "I'll get back to you soon."
           };
           self.formTarget.insertAdjacentHTML('beforebegin', alert_template(context));
           self.disconnect();
@@ -530,6 +531,73 @@ function (_Controller) {
 }(stimulus__WEBPACK_IMPORTED_MODULE_0__["Controller"]);
 
 _default.targets = ['form', 'input', 'alert'];
+
+
+/***/ }),
+
+/***/ "./_src/controllers/toggle_controller.js":
+/*!***********************************************!*\
+  !*** ./_src/controllers/toggle_controller.js ***!
+  \***********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return _default; });
+/* harmony import */ var stimulus__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! stimulus */ "./node_modules/stimulus/index.js");
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+
+
+var _default =
+/*#__PURE__*/
+function (_Controller) {
+  _inherits(_default, _Controller);
+
+  function _default() {
+    _classCallCheck(this, _default);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(_default).apply(this, arguments));
+  }
+
+  _createClass(_default, [{
+    key: "connect",
+    value: function connect() {
+      this.toggleClass = this.data.get('class') || 'hidden';
+    }
+  }, {
+    key: "toggle",
+    value: function toggle(event) {
+      var _this = this;
+
+      event.preventDefault();
+      this.toggleableTargets.forEach(function (target) {
+        target.classList.toggle(_this.toggleClass);
+      });
+    }
+  }]);
+
+  return _default;
+}(stimulus__WEBPACK_IMPORTED_MODULE_0__["Controller"]);
+
+_default.targets = ['toggleable'];
 
 
 /***/ }),
